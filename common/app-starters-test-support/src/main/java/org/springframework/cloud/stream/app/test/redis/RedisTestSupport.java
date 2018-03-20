31b8e65e56f484c3d822675844809205c83e6a95
Soby Chacko <schacko@pivotal.io>
2018-03-20 12:53:41 -0400

    RedisTestSupport changes
    Use Lettuce driver in RedisTestSupport instead of the Jedis driver that was in use before
    Boot 2.0 spring data redis starter uses Lettuce instead of Jedis and this causes CNF excpetions
    in downstream app starters if we keep Jedis as the driver in RedisTestSupport. Updating the
    driver to Lettuce.

--- common/app-starters-test-support/src/main/java/org/springframework/cloud/stream/app/test/redis/RedisTestSupport.java
+++ common/app-starters-test-support/src/main/java/org/springframework/cloud/stream/app/test/redis/RedisTestSupport.java
@@ -17,14 +17,14 @@
 package org.springframework.cloud.stream.app.test.redis;
 
 import org.springframework.cloud.stream.test.junit.AbstractExternalResourceTestSupport;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 /**
  * Porting from https://github.com/spring-cloud/spring-cloud-stream/blob/1.0.x/spring-cloud-stream-test-support-internal
  * 
  * @author Soby Chacko
  */
-public class RedisTestSupport extends AbstractExternalResourceTestSupport<JedisConnectionFactory> {
+public class RedisTestSupport extends AbstractExternalResourceTestSupport<LettuceConnectionFactory> {
 
 	public RedisTestSupport() {
 		super("REDIS");
@@ -36,7 +36,7 @@ public class RedisTestSupport extends AbstractExternalResourceTestSupport<JedisC
 
 	@Override
 	protected void obtainResource() throws Exception {
-		resource = new JedisConnectionFactory();
+		resource = new LettuceConnectionFactory();
 		resource.afterPropertiesSet();
 		resource.getConnection().close();
 	}

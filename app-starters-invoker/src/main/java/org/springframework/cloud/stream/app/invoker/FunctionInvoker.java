8d45f84c6af3ba752cfa531d869ffc6426366561
Soby Chacko <schacko@pivotal.io>
2018-05-23 18:12:20 -0400

    apps to function spike

---
+++ app-starters-invoker/src/main/java/org/springframework/cloud/stream/app/invoker/FunctionInvoker.java
@@ -0,0 +1,25 @@
+package org.springframework.cloud.stream.app.invoker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+
+import java.util.function.Function;
+
+/**
+ * @author Soby Chacko
+ */
+@EnableBinding(Processor.class)
+public class FunctionInvoker {
+
+	@Autowired(required = false)
+	Function<Message<?>,?> functionToInvoke;
+
+	@ServiceActivator(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
+	public Object invokeProcessor(Message<?> message) {
+		return functionToInvoke != null ? functionToInvoke.apply(message) : null;
+	}
+
+}

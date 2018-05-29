84197d35999048f255b6b94a4866099573d2bd40
Soby Chacko <schacko@pivotal.io>
2018-05-29 15:53:07 -0400

    cleanup

--- app-starters-invoker/src/main/java/org/springframework/cloud/stream/app/invoker/FunctionalIntegrationConfiguration.java
+++ app-starters-invoker/src/main/java/org/springframework/cloud/stream/app/invoker/FunctionalIntegrationConfiguration.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
 
 import java.util.Map;
 import java.util.function.Consumer;
@@ -28,16 +29,19 @@ import java.util.function.Supplier;
 @Configuration
 public class FunctionalIntegrationConfiguration {
 
-	@Conditional(FunctionCondition.class)
+	//Need to explore ordering for autoconfiguration so that FunctionConidtion waits for
+	//any downstream function beans to be registered.
+
+	//@Conditional(FunctionCondition.class)
 	@Configuration
 	@EnableBinding(Processor.class)
 	public static class FunctionInvoker {
 
 		@Autowired
-		Function<?, ?> functionToInvoke;
+		Function<Message<?>, Object> functionToInvoke;
 
 		@ServiceActivator(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
-		public Object invokeProcessor(Object o) {
+		public Object invokeProcessor(Message<?> o) {
 			return functionToInvoke.apply(o);
 		}
 	}
@@ -72,5 +76,63 @@ public class FunctionalIntegrationConfiguration {
 
 	//Need to have another kind of Source invoker that uses IntegrationFlow in many app starters like ftp, file, jdbc etc.
 
+	private static class FunctionCondition extends SpringBootCondition {
+
+		public FunctionCondition() {
+		}
+
+		@Override
+		public ConditionOutcome getMatchOutcome(ConditionContext context, AnnotatedTypeMetadata metadata) {
+			try {
+				Function function = context.getBeanFactory().getBean(Function.class);
+				//check function is qualified
+			} catch (NoSuchBeanDefinitionException nsbde) {
+				return ConditionOutcome.noMatch("Function bean is not found");
+			}
+			return ConditionOutcome.match("Function bean is found");
+		}
+	}
+
+	private static class ConsumerCondition extends SpringBootCondition {
+
+		public ConsumerCondition() {}
+
+		@Override
+		public ConditionOutcome getMatchOutcome(ConditionContext context, AnnotatedTypeMetadata metadata) {
+			try {
+				Consumer consumer = context.getBeanFactory().getBean(Consumer.class);
+			} catch (NoSuchBeanDefinitionException nsbde) {
+				return ConditionOutcome.noMatch("Consumer bean is not found");
+			}
+			return ConditionOutcome.match("Consumer bean is found");
+		}
+	}
+
+	private static class PollableSourceCondition extends SpringBootCondition {
+
+		public PollableSourceCondition() {}
+
+		@Override
+		public ConditionOutcome getMatchOutcome(ConditionContext context, AnnotatedTypeMetadata metadata) {
+			try {
+				Map<String, Object> pollableBeans = context.getBeanFactory().getBeansWithAnnotation(PollableSupplier.class);
+
+				if (!pollableBeans.isEmpty()) {
+					Object next = pollableBeans.values().iterator().next();
+					if (next.getClass().isAssignableFrom(Supplier.class)) {
+						return ConditionOutcome.match("Pollable source bean is found");
+					}
+					else {
+						return ConditionOutcome.noMatch("Pollable source bean is not found");
+					}
+				}
+				else {
+					return ConditionOutcome.noMatch("Pollable source bean is not found");
+				}
+			} catch (NoSuchBeanDefinitionException nsbde) {
+				return ConditionOutcome.noMatch("Pollable source bean is not found");
+			}
+		}
+	}
 
 }
